21c1d93d7a4d3b1bd9174c2c72ecae0dcf4388b3
xjccc <546534045@qq.com>
2025-05-09 14:01:44 +0800

    chore: lint

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -1,11 +1,15 @@
 import type { MaybeRef } from '#imports'
 import type { CustomFetchOptions, FetchMethod } from './type'
-import { toValue } from '#imports'
+import { reactive, toValue } from '#imports'
+import { isPlainObject } from '@vue/shared'
 import { hash } from 'ohash'
 
 export function Noop () { }
-export function generateOptionSegments (opts: CustomFetchOptions & { method: FetchMethod }) {
-  const segments: Array<string | undefined | Record<string, string>> = [toValue(opts.method as MaybeRef<string | undefined> | undefined)?.toUpperCase() || 'GET', toValue(opts.baseURL)]
+export function generateOptionSegments<_ResT> (opts: CustomFetchOptions & { method: FetchMethod }) {
+  const segments: Array<string | undefined | Record<string, string>> = [
+    toValue(opts.method as MaybeRef<string | undefined> | undefined)?.toUpperCase() || 'GET',
+    toValue(opts.baseURL)
+  ]
   for (const _obj of [opts.params || opts.query]) {
     const obj = toValue(_obj)
     if (!obj) {
@@ -16,11 +20,35 @@ export function generateOptionSegments (opts: CustomFetchOptions & { method: Fet
     for (const [key, value] of Object.entries(obj)) {
       unwrapped[toValue(key)] = toValue(value)
     }
-
     segments.push(unwrapped)
   }
   if (opts.body) {
-    segments.push(hash(toValue(opts.body)))
+    const value = toValue(opts.body)
+    if (!value) {
+      segments.push(hash(value))
+    }
+    else if (value instanceof ArrayBuffer) {
+      segments.push(hash(Object.fromEntries([...new Uint8Array(value).entries()].map(([k, v]) => [k, v.toString()]))))
+    }
+    else if (value instanceof FormData) {
+      const obj: Record<string, string> = {}
+      for (const entry of value.entries()) {
+        const [key, val] = entry
+        obj[key] = val instanceof File ? val.name : val
+      }
+      segments.push(hash(obj))
+    }
+    else if (isPlainObject(value)) {
+      segments.push(hash(reactive(value)))
+    }
+    else {
+      try {
+        segments.push(hash(value))
+      }
+      catch {
+        console.warn('[useFetch] Failed to hash body', value)
+      }
+    }
   }
   return segments
 }
